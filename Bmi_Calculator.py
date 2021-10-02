7eeef2f300e84e85c427b35bfe7c4a4cbfbeef9c
tooshminator <81343678+tooshminator@users.noreply.github.com>
2021-10-03 01:08:21 +0530

    Add files via upload

---
+++ Bmi_Calculator.py
@@ -0,0 +1,74 @@
+name1 = "Ojas"   #PLACE NAME HERE
+height1 = 1.71   #PLACE HEIGHT HERE
+weight1 = 2     #PLACE WEIGHT HERE
+bmi1 = weight1/(height1*height1)
+if bmi1 < 18.5:
+    print ("bmi:")
+    print (bmi1)
+    print (name1)
+    print ("Is underweight")
+    print ("Come on bro put some ghee in your food")
+elif bmi1 > 25:
+    print("bmi:")
+    print(bmi1)
+    print (name1)
+    print ("Is overweight")
+    print ("Excercises excercises we must do our excercises")
+else:
+    print ("bmi:")
+    print (bmi1)
+    print (name1)
+    print ("Has healthy weight")
+    print ("Let's go man, keep it up!")
+
+
+
+
+name2 = 'Ayush'   #PLACE NAME HERE
+height2 = 1.71   #PLACE HEIGHT HERE
+weight2 = 2   #PLACE WEIGHT HERE
+bmi2 = weight2/(height2*height2)
+if bmi2 < 18.5:
+    print ('bmi:')
+    print (bmi2)
+    print (name2)
+    print ("Is underweight")
+    print("Come on bro put some ghee in your food")
+elif bmi2 > 25:
+    print("bmi:")
+    print(bmi2)
+    print(name2)
+    print("Is overweight")
+    print ("Excercises excercises we must do our excercises")
+else:
+    print ('bmi:')
+    print (bmi2)
+    print(name2)
+    print('Has healthy weight')
+    print("Let's go man, keep it up")
+
+
+
+
+name3 = 'yyy'   #PLACE NAME HERE
+height3 =1.71    #PLACE HEIGHT HERE
+weight3 =2     #PLACE WEIGHT HERE
+bmi3 = weight3/ (height3*height3)
+if bmi3 < 18.5:
+    print('bmi:')
+    print(bmi3)
+    print (name3)
+    print ('Is underweight')
+    print("Come on bro put some ghee in your food")
+elif bmi3 > 25:
+    print("bmi:")
+    print(bmi3)
+    print(name3)
+    print("Is overweight")
+    print("Excercises excercises we must do our excercises")
+else:
+    print('bmi:')
+    print(bmi3)
+    print (name3)
+    print ('Has healthy weight')
+    print("Let's go man, keep it up")
