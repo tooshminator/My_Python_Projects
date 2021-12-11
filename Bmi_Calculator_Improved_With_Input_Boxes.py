9e55b16bd089572b733a04202e2c7a5024dca81e
tooshminator <81343678+tooshminator@users.noreply.github.com>
2021-12-12 02:02:29 +0530

    Add files via upload

---
+++ Bmi_Calculator_Improved_With_Input_Boxes.py
@@ -0,0 +1,74 @@
+name1 = input("Enter your name here:")
+height1 = float(input ("Enter your height here in meters:"))
+weight1 = float(input ("Enter your weight here in kilograms: "))
+bmi1 = weight1/(height1*height1)
+if bmi1 < 18.5:
+    print ("bmi:")
+    print (bmi1)
+    print (name1)
+    print ("Is underweight")
+    print ("Come on bro put some ghee in your food")
+elif bmi1 > 25:
+    print("bmi:")
+    print(bmi1)
+    print (name1)
+    print ("Is overweight")
+    print ("Excercises excercises we must do our excercises")
+else:
+    print ("bmi:")
+    print (bmi1)
+    print (name1)
+    print ("Has healthy weight")
+    print ("Let's go man, keep it up!")
+
+
+
+
+name2 = input("Enter your name here:")
+height2 =float(input("Enter your height here in meters:"))
+weight2 =float(input("Enter your weight here kilograms: "))
+bmi2 = weight2/(height2*height2)
+if bmi2 < 18.5:
+    print ('bmi:')
+    print (bmi2)
+    print (name2)
+    print ("Is underweight")
+    print("Come on bro put some ghee in your food")
+elif bmi2 > 25:
+    print("bmi:")
+    print(bmi2)
+    print(name2)
+    print("Is overweight")
+    print ("Excercises excercises we must do our excercises")
+else:
+    print ('bmi:')
+    print (bmi2)
+    print(name2)
+    print('Has healthy weight')
+    print("Let's go man, keep it up")
+
+
+
+
+name3 = input("Enter your name here:")
+height3 =float(input ("Enter your height here in meters:"))
+weight3 =float(input ("Enter your weight here in kilograms: "))
+bmi3 = weight3/ (height3*height3)
+if bmi3 < 18.5:
+    print('bmi:')
+    print(bmi3)
+    print (name3)
+    print ('Is underweight')
+    print("Come on bro put some ghee in your food")
+elif bmi3 > 25:
+    print("bmi:")
+    print(bmi3)
+    print(name3)
+    print("Is overweight")
+    print("Excercises excercises we must do our excercises")
+else:
+    print('bmi:')
+    print(bmi3)
+    print (name3)
+    print ('Has healthy weight')
+    print("Let's go man, keep it up")
